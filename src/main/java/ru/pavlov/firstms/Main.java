77b2c122a7613f67b8ddabbffc638eb4ee56b394
antonpavlov <anton.it.pavlov@gmail.com>
2018-02-01 01:43:38 +0300

    commit

--- src/main/java/ru/pavlov/firstms/Main.java
+++ src/main/java/ru/pavlov/firstms/Main.java
@@ -1,6 +1,8 @@
 package ru.pavlov.firstms;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import spark.Request;
 
 import java.io.UnsupportedEncodingException;
@@ -13,31 +15,63 @@ import static spark.Spark.*;
 public class Main {
 
     private static Set<String> sessions = new HashSet<>();
-    private static List<Car> carList = new ArrayList<>();
+    private static List<Car> cars = new ArrayList<>();
 
     public static void main(String[] args) {
         port(7676);
-        patch("/data", () -> {
+
+        path("/data", () -> {
             before("/*", (request, response) -> {
                 String session = getSession(request);
                 if (!sessions.contains(session)) {
                     halt(401, "Not Authorization!!!");
                 }
             });
-
-            get("/getAllCars", (((request, response) -> new Gson().toJson(carList))));
-
+            get("/getAllCars", (((request, response) -> new Gson().toJson(cars))));
             after("/*", ((request, response) -> {
                 //can you make something.
             }));
 
             post("/add", ((request, response) -> {
                 String body = request.body();
-                if(body!=null){
-                    String str = URLDecoder.decode(body, StandardCharsets.UTF_8.name());
+                if (body != null) {
+                    String message = URLDecoder.decode(body, StandardCharsets.UTF_8.name());
+                    Car car = new Gson().fromJson(message, Car.class);
+                    cars.add(car);
                 }
+                response.status(200);
+                return "success";
             }));
         });
+
+        post("/login", ((request, response) -> {
+            String body = request.body();
+            if (body != null) {
+                String message = URLDecoder.decode(body, StandardCharsets.UTF_8.name());
+                message = message.replace("=", "");
+                JsonObject jsonObject = (JsonObject) new JsonParser().parse(message);
+                String userName = jsonObject.get("userName").getAsString();
+                String password = jsonObject.get("password").getAsString();
+                if ("demo".equals(userName) && "demo".equals(password)) {
+                    sessions.add(getSession(request));
+                    response.status(200);
+                    return "success";
+                }
+                halt(404, "Invalid credentials");
+            }
+            return "Invalid failed";
+        }));
+
+        post("/logout", ((request, response) -> {
+            String session = getSession(request);
+            if (sessions.contains(session)) {
+                sessions.remove(session);
+            }
+            response.status(200);
+            return "success";
+        }));
+
+        get("/test",((request, response) -> "Test success"));
     }
 
     private static String getSession(Request request) throws UnsupportedEncodingException {
